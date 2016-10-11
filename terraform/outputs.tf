2194ece56ba5cf2e190ab0909512d0b02cf9a076
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-11 10:09:42 -0400

    refactor

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,11 @@
 output "public_server_ips" {
   value = ["${aws_instance.server.*.public_ip}"]
 }
+
 output "private_server_ips" {
   value = ["${aws_instance.server.*.private_ip}"]
+}
+
+output "web_security_group_ids" {
+  value = ["${aws_security_group.consul-ui.id}"]
 }
