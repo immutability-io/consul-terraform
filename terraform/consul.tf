ab758595bb7a1c533954a5a30b4c461d457290e6
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-11 16:41:48 -0400

    SSL

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -85,8 +85,8 @@ resource "aws_security_group" "consul"
 
     ingress
     {
-        from_port = 80
-        to_port = 80
+        from_port = 443
+        to_port = 443
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
