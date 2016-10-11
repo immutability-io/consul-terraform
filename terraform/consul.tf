bbe0b5b3785fbc8f852b485bc6eb5a7fd55f3102
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-11 10:21:26 -0400

    Security group

--- terraform/consul.tf
+++ terraform/consul.tf
@@ -71,61 +71,6 @@ resource "aws_security_group" "consul"
 
     ingress
     {
-        from_port = 0
-        to_port = 65535
-        protocol = "udp"
-        security_groups = ["${aws_security_group.consul-ui.id}"]
-    }
-
-    ingress
-    {
-        from_port = 0
-        to_port = 65535
-        protocol = "tcp"
-        security_groups = ["${aws_security_group.consul-ui.id}"]
-    }
-
-    ingress
-    {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["${var.ingress_22}"]
-    }
-
-    egress
-    {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-resource "aws_security_group" "consul-ui"
-{
-    name = "${var.tagName}-ui-security-group"
-    description = "Consul web traffic + maintenance."
-    vpc_id = "${var.vpc_id}"
-
-    ingress
-    {
-        from_port = 0
-        to_port = 65535
-        protocol = "udp"
-        security_groups = ["${aws_security_group.consul.id}"]
-    }
-
-    ingress
-    {
-        from_port = 0
-        to_port = 65535
-        protocol = "tcp"
-        security_groups = ["${aws_security_group.consul.id}"]
-    }
-
-    ingress
-    {
         from_port = 22
         to_port = 22
         protocol = "tcp"
