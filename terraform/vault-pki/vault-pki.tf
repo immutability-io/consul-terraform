421fe25bdf9075cbba4ff784cf043512639a031d
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-20 16:10:24 -0400

    local exec hassle

--- terraform/vault-pki/vault-pki.tf
+++ terraform/vault-pki/vault-pki.tf
@@ -1,7 +1,7 @@
 
 resource "null_resource" "issue-certificate" {
     provisioner "local-exec" {
-        command = << EOT
+        command = <<EOT
         VAULT_TOKEN=${var.vault-token}
         vault write -format=json vault_intermediate/issue/web_server common_name="${var.common-name}"  ip_sans="${var.ip-sans}" ttl=720h > ./tmp.json
 EOT
