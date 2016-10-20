43bcb2c2aebb2ebf8274eaec087d6efe0107c950
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-20 18:49:40 -0400

    Vault module

--- terraform/vault-pki/vault-pki.tf
+++ terraform/vault-pki/vault-pki.tf
@@ -5,4 +5,13 @@ resource "null_resource" "issue-certificate" {
         VAULT_TOKEN=${var.vault-token} vault write -address=${var.vault-addr} -format=json vault_intermediate/issue/web_server common_name="${var.common-name}"  ip_sans="${var.ip-sans}" ttl=720h > ./tmp.json
 EOT
     }
+    provisioner "local-exec" {
+        command = "cat ./tmp.json | jq -r .data.certificate | cat > ./cert.crt"
+    }
+    provisioner "local-exec" {
+        command = "cat ./tmp.json | jq -r .data.issuing_ca | cat > ./issuer.crt"
+    }
+    provisioner "local-exec" {
+        command = "cat ./tmp.json | jq -r .data.private_key | cat > ./private.key"
+    }
 }
