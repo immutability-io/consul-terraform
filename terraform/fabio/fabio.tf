358198204bf87b4751344fcac85d56c4465798d0
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-27 16:07:36 -0400

    Tried using DNS for load balancing because ELBs cost money

--- terraform/fabio/fabio.tf
+++ terraform/fabio/fabio.tf
@@ -29,6 +29,20 @@ resource "aws_security_group" "fabio" {
         cidr_blocks = ["${var.vpc_cidr}"]
     }
 
+    ingress {
+        from_port = 9999
+        to_port = 9999
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
--- terraform/fabio/outputs.tf
+++ terraform/fabio/outputs.tf
@@ -3,6 +3,10 @@ output "private_server_ips" {
   value = ["${aws_instance.fabio.*.private_ip}"]
 }
 
+output "public_server_ips" {
+  value = ["${aws_instance.fabio.*.public_ip}"]
+}
+
 output "instance_ids" {
   value = ["${aws_instance.fabio.*.id}"]
 }
