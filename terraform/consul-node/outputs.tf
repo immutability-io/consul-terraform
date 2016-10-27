17304e2a6766b74efa35b736e469ea1a50600210
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-10-27 14:52:49 -0400

    Removed Public IPs

--- terraform/consul-node/outputs.tf
+++ terraform/consul-node/outputs.tf
@@ -1,6 +1,3 @@
-output "public_server_ips" {
-  value = ["${aws_instance.consul-node.*.public_ip}"]
-}
 
 output "private_server_ips" {
   value = ["${aws_instance.consul-node.*.private_ip}"]
--- terraform/fabio/outputs.tf
+++ terraform/fabio/outputs.tf
@@ -1,8 +1,4 @@
 
-output "public_server_ips" {
-  value = ["${aws_instance.fabio.*.public_ip}"]
-}
-
 output "private_server_ips" {
   value = ["${aws_instance.fabio.*.private_ip}"]
 }
