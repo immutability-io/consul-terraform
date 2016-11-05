cacaae79bf74afac9dda361b2b225180e9a2f112
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-11-05 16:57:33 -0400

    Add variable to vault-pki

--- terraform/vault-pki/vault-pki.tf
+++ terraform/vault-pki/vault-pki.tf
@@ -2,19 +2,19 @@
 resource "null_resource" "issue-certificate" {
     provisioner "local-exec" {
         command = <<EOT
-        VAULT_TOKEN=${var.vault_token} vault write -address=${var.vault_addr} -format=json vault_intermediate/issue/web_server common_name="${var.common_name}" alt_names="${var.alt_names}" ip_sans="${var.ip_sans}" ttl=720h > ./tmp.json
+        VAULT_TOKEN=${var.vault_token} vault write -address=${var.vault_addr} -format=json vault_intermediate/issue/web_server common_name="${var.common_name}" alt_names="${var.alt_names}" ip_sans="${var.ip_sans}" ttl=720h > ${var.temp_file}
 EOT
     }
     provisioner "local-exec" {
-        command = "cat ./tmp.json | jq -r .data.certificate | cat > ${var.certificate}"
+        command = "cat ${var.temp_file} | jq -r .data.certificate | cat > ${var.certificate}"
     }
     provisioner "local-exec" {
-        command = "cat ./tmp.json | jq -r .data.issuing_ca | cat > ${var.issuer_certificate}"
+        command = "cat ${var.temp_file} | jq -r .data.issuing_ca | cat > ${var.issuer_certificate}"
     }
     provisioner "local-exec" {
-        command = "cat ./tmp.json | jq -r .data.private_key | cat > ${var.private_key}"
+        command = "cat ${var.temp_file} | jq -r .data.private_key | cat > ${var.private_key}"
     }
     provisioner "local-exec" {
-        command = "rm ./tmp.json"
+        command = "rm ${var.temp_file}"
     }
 }
