0ff6d7512b985c1403fa79665c2659354338793b
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-11-05 15:52:41 -0400

    Vault module

---
+++ terraform/vault-service/scripts/install_vault_service.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -d "/etc/consul.d" ]; then
+  sudo mkdir /etc/consul.d
+fi
+if [ ! -d "/etc/vault.d" ]; then
+  sudo mkdir /etc/vault.d
+fi
+if [ ! -d "/etc/service" ]; then
+  sudo mkdir /etc/service
+fi
+
+echo "Registering Vault with Consul..."
+sudo chmod 777 /tmp/vault_service.json
+sudo mv /tmp/vault_service.json /etc/consul.d
+
+echo "Configure Vault ..."
+sudo chmod 777 /tmp/vault_service.json
+sudo mv /tmp/vault_service.json /etc/consul.d
+
+sudo chmod 777 /tmp/vault.json
+sed -i "s/vault_ip/`ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }'`/g" /tmp/vault.json
+sudo mv /tmp/vault.json /etc/vault.d
+
+echo "Installing Vault Upstart..."
+sudo chown root:root /tmp/vault.conf
+sudo mv /tmp/vault.conf /etc/init/vault.conf
+sudo chmod 0644 /etc/init/vault.conf
+
+echo "Installing Consul data directory..."
+sudo mkdir -p /opt/consul/data
+
+
+# Write the flags to a temporary file
+cat >/tmp/consul_flags << EOF
+CONSUL_FLAGS="-client"
+EOF
+
+echo "Installing Consul Upstart service..."
+sudo chown root:root /tmp/upstart.conf
+sudo mv /tmp/upstart.conf /etc/init/consul.conf
+sudo chmod 0644 /etc/init/consul.conf
+sudo mv /tmp/consul_flags /etc/service/consul
+sudo chmod 0644 /etc/service/consul
---
+++ terraform/vault-service/vault-service.tf
@@ -0,0 +1,150 @@
+
+data "template_file" "template-consul-client-config" {
+    template = "${file("${path.module}/config/client_consul_config.tpl")}"
+    vars {
+        retry_join = "${join("\",\"", var.consul_cluster_ips)}"
+        datacenter = "${var.datacenter}"
+        gossip_encryption_key = "${var.gossip_encryption_key}"
+    }
+}
+
+
+resource "aws_instance" "consul-service"
+{
+    ami = "${var.ami}"
+    count = "${var.service_count}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.key_name}"
+    subnet_id = "${var.subnet_id}"
+    associate_public_ip_address = "${var.associate_public_ip_address}"
+    vpc_security_group_ids = ["${aws_security_group.consul-service.id}"]
+
+    connection
+    {
+        user          = "ubuntu"
+        host          = "${self.private_ip}"
+        private_key   = "${var.private_key}"
+        agent         = "false"
+        bastion_host  = "${var.bastion_public_ip}"
+        bastion_user  = "${var.bastion_user}"
+    }
+
+    tags
+    {
+        Name = "${var.tagName}-${count.index}"
+        Finance = "${var.tagFinance}"
+        OwnerEmail = "${var.tagOwnerEmail}"
+        Schedule = "${var.tagSchedule}"
+        BusinessJustification = "${var.tagBusinessJustification}"
+        AutoStart = "${var.tagAutoStart}"
+    }
+
+    provisioner "file" {
+        content = "${data.template_file.template-consul-client-config.rendered}"
+        destination = "/tmp/consul.json"
+    }
+
+    provisioner "file" {
+        source = "${path.module}/config/vault_service.json"
+        destination = "/tmp/vault_service.json"
+    }
+
+    provisioner "file" {
+        source = "${path.module}/config/vault.json"
+        destination = "/tmp/vault.json"
+    }
+
+    provisioner "file" {
+        source = "${var.root_certificate}"
+        destination = "/tmp/root.crt"
+    }
+
+    provisioner "file" {
+        source = "${var.consul_certificate}"
+        destination = "/tmp/consul.crt"
+    }
+
+    provisioner "file" {
+        source = "${var.consul_key}"
+        destination = "/tmp/consul.key"
+    }
+
+    provisioner "file" {
+        source = "${var.vault_certificate}"
+        destination = "/tmp/vault.crt"
+    }
+
+    provisioner "file" {
+        source = "${var.vault_key}"
+        destination = "/tmp/vault.key"
+    }
+
+    provisioner "file" {
+        source = "${path.module}/config/rest_service.conf"
+        destination = "/tmp/rest_service.conf"
+    }
+
+    provisioner "file" {
+        source = "${path.module}/config/upstart.conf"
+        destination = "/tmp/upstart.conf"
+    }
+
+    provisioner "file" {
+        source = "${path.module}/config/vault.conf"
+        destination = "/tmp/vault.conf"
+    }
+
+    provisioner "remote-exec" {
+        scripts = [
+            "${path.module}/scripts/stop_nginx.sh"
+        ]
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x /tmp/install_vault_service.sh",
+            "/tmp/install_vault_service.sh"
+        ]
+    }
+
+    provisioner "file" {
+        source = "${path.module}/config/dnsmasq.conf"
+        destination = "/tmp/dnsmasq.conf"
+    }
+
+    provisioner "remote-exec" {
+        scripts = [
+            "${path.module}/scripts/setup_certs.sh",
+            "${path.module}/scripts/vault_service.sh",
+            "${path.module}/scripts/dnsmasq.sh"
+        ]
+    }
+}
+resource "aws_security_group" "vault-service" {
+    name = "${var.tagName}-security-group"
+    description = "Vault internal traffic + maintenance."
+    vpc_id = "${var.vpc_id}"
+
+    // These are for internal traffic
+
+    ingress {
+        protocol    = -1
+        from_port   = 0
+        to_port     = 0
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+
+    ingress {
+        protocol    = "tcp"
+        from_port   = 8080
+        to_port     = 8080
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
