a0b7527812bb37f3213df4a1596ca0909fc7f5ca
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-11-28 15:47:51 -0500

    Check SHA sums

--- packer/scripts/install_hashistack.sh
+++ packer/scripts/install_hashistack.sh
@@ -17,7 +17,9 @@ function grab {
   fi
   unzip ./$1.zip
   sudo mv ./$1 /usr/local/bin/$1
-  rm ./$1_$2_SHA256SUMS.sig 
+  sudo chmod 0755 /usr/local/bin/$1
+  sudo chown root:root /usr/local/bin/$1
+  rm ./$1_$2_SHA256SUMS.sig
   rm ./$1_$2_SHA256SUMS
   rm ./$1.zip
 }
@@ -25,9 +27,8 @@ function grab {
 echo -e "\n[hashi tools] grabbing...\n";
 
 grab vault 0.6.2
-grab terraform 0.8.0-rc1
 grab consul 0.7.1
 grab nomad 0.5.0
-grab packer 0.12.0
-
-echo -e "\n[hashi tools] grabbing completed ;) \n";
+grab consul-template 0.16.0
+grab consul-replicate 0.2.0
+grab envconsul 0.6.1
