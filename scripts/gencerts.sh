c33cae3f72447bbe4400cbe5cafb1d863118f269
Jeff Ploughman <jeff_ploughman@troweprice.com>
2016-11-29 13:01:20 -0500

    Compacting...

--- scripts/gencerts.sh
+++ scripts/gencerts.sh
@@ -1,7 +1,7 @@
 touch certindex
 echo "000a" > serialfile
 CA_SUBJ="/C=US/ST=Maryland/L=Baltimore/O=Immutability, LLC/OU=Infrastructure Automation/CN=My Development CA"
-openssl req -newkey rsa:2048 -days 3650 -x509 -nodes -subj "${CA_SUBJ}" -out root.cer
+openssl req -newkey rsa:2048 -days 3650 -x509 -nodes -subj "${CA_SUBJ}" -out root.crt
 SUBJ="/C=US/ST=Maryland/L=Baltimore/O=Immutability, LLC/OU=Infrastructure Automation/CN=*.immutability.io"
 openssl req -newkey rsa:2048 -nodes -subj  "${SUBJ}"  -out hashicorp.csr -keyout hashicorp.key
 openssl ca -batch -config hashicorp_ca.conf -notext -in hashicorp.csr -out hashicorp.cer
